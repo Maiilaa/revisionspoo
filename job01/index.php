9b727d253686e65eebbb05a560c0e7812a067765
Maiilaa <zirarimaila8@gmail.com>
2024-09-04 11:18:20 +0200

    creation de la classe + variables

--- job01/index.php
+++ job01/index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <header>
-        <h1>coucou</h1>
+      
     </header>
     <main>
 
---
+++ job01/product.php
@@ -0,0 +1,17 @@
+<?php 
+    class Product {
+        private $id;
+        public $name;
+        public $photos;
+        private $price;
+        public $description;
+        private $quantity;
+        private $createdAt;
+        private $updatedAt;
+    }
+
+
+
+
+
+?>
